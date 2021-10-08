430276b60f32a37f3d3a952a10b93dfb26e2a874
Developer0772 <alishernarzullayev7777@gmail.com>
2021-10-08 12:43:25 +0500

    commit

--- src/pages/upload/upload.js
+++ src/pages/upload/upload.js
@@ -1,24 +1,33 @@
 import { CloudUpload} from "@material-ui/icons";
+import axios from "axios";
 import Topbar from "../../components/topbar/topbar";
 import "./upload.css"
 const Upload = () => {
     const filePick = (e)=>{
        const file = e.target.files[0];
-      return document.getElementById('filename').innerText = "File: "+file.name;
+       const form = new FormData();
+       form.append(
+           "file",file,file.name
+       );
+      document.getElementById('filename').innerText = "File: "+file.name;
+      return form
+    }
+    const onSubmit = ()=>{
+        axios.post('api/download',filePick)
     }
     return ( 
         <>
         <Topbar/>
         <div className="uploadPage">
             <h2 className="header">Yuborish:</h2>
-            <form className="uploadForm" action="http://localhost:3000" method="post" encType="multipart/form-data">
+            <form className="uploadForm" method="post" encType="multipart/form-data">
                 <div className="uploadIcon">
                 <input onChange={filePick} type="file" required name="subject" id="file" multiple="multiple" accept=".pdf, .docx, .doc, .ppt, .pptx, .xls, .xlsx, .zip, .rar"/>
                 <CloudUpload  className="fileIcon"/>
                 <p className="filename" id="filename"></p>
                 </div>
                 <textarea name="text" id="desc" required placeholder="Izoh:"></textarea>
-                <button className="submitBtn" type="submit">Yuborish</button>
+                <button onClick={onSubmit} className="submitBtn" type="submit">Yuborish</button>
             </form>
         </div>
         </>
